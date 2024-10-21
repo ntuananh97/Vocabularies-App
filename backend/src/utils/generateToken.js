bcde01c07dd36c743d652cac9e17cd546edaad48
tuananh <anh.nguyentuan@euroland.com>
2024-10-21 14:16:48 +0700

    fix (BE): edit option when server set cookie after login successfully

--- backend/src/utils/generateToken.js
+++ backend/src/utils/generateToken.js
@@ -2,7 +2,7 @@ const jwt = require("jsonwebtoken");
 const process = require("process");
 const { TOKEN_NAME } = require("../configs/constants");
 const { checkProduction } = require("../utils");
-const { DOMAIN } = require("../configs/appConfig");
+// const { DOMAIN } = require("../configs/appConfig");
 
 
 const generateAuthToken = (payload = {}) => {
@@ -28,11 +28,12 @@ const generateTokenAndSetCookie = (payload, res) => {
     httpOnly: true,
     secure: checkProduction() ? true : false,
     maxAge: 15 * 24 * 60 * 60 * 1000, // 15 days
-    sameSite: checkProduction() ? "None" : "Strict",
+    sameSite: "Strict",
+    // sameSite: checkProduction() ? "None" : "Strict",
     path: "/",
   }
 
-  if (checkProduction()) options.domain = DOMAIN.PRODUCTION;
+  // if (checkProduction()) options.domain = DOMAIN.PRODUCTION;
 
   res.cookie(TOKEN_NAME, token, options);
 };
